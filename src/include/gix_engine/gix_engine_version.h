1e0831f7bb984845ffc127e9f8b6bb441b2c59bc
Yogi Astawan <yogi.astawan@gmail.com>
2025-06-06 22:57:58 +0800

    update version macro

--- src/include/gix_engine/gix_engine_version.h
+++ src/include/gix_engine/gix_engine_version.h
@@ -5,14 +5,24 @@
 extern "C" {
 #endif
 
+#ifndef GIX_ENGINE_MAJOR_VERSION
 #define GIX_ENGINE_MAJOR_VERSION 0
+#endif
+#ifndef GIX_ENGINE_MINOR_VERSION
 #define GIX_ENGINE_MINOR_VERSION 1
+#endif
+
+#ifndef GIX_ENGINE_PATCH_VERSION
 #define GIX_ENGINE_PATCH_VERSION 0
+#endif
+
+#ifndef GIX_ENGINE_VERSION
+#define GIX_ENGINE_VERSION "0.1.0"
+#endif
 
 #define GIX_ENGINE_VERSION_NUM                                            \
     ((GIX_ENGINE_MAJOR_VERSION << 16) | (GIX_ENGINE_MINOR_VERSION << 8) | \
      GIX_ENGINE_PATCH_VERSION)
-#define GIX_ENGINE_VERSION "0.1.0"
 
 /**
  * @brief Get version of Gix Engine in string.
