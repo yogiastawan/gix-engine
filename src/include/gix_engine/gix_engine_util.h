4fbc36911e54e124f9690080617de7e182cd9e98
Yogi Astawan <yogi.astawan@gmail.com>
2025-06-06 18:47:37 +0800

    add utils header

---
+++ src/include/gix_engine/gix_engine_util.h
@@ -0,0 +1,14 @@
+#ifndef __GIX_ENGINE_UTIL_H__
+#define __GIX_ENGINE_UTIL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define gix_array(t, arr...) (t) arr
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GIX_ENGINE_UTIL_H__ */
