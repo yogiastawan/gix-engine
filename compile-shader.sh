333c5c91f77b990870decbfed9e8b66423d67d8e
Yogi Astawan <yogi.astawan@gmail.com>
2025-06-10 01:05:38 +0800

    fix shader compile

--- compile-shader.sh
+++ compile-shader.sh
@@ -1,16 +1,37 @@
+set -e
+
 mkdir -p ${DST}/SPIRV
 mkdir -p ${DST}/MSL
 mkdir -p ${DST}/DXIL
 
 for filename in $SRC/*.vert.hlsl; do
+    res=0
     if [ -f "$filename" ]; then
         echo -e "\e[94mCompile file: \e[94m${filename}\e[0m"
         BASE_NAME=$(basename ${filename})
         $COMPILER "$filename" -o "${DST}/SPIRV/${BASE_NAME/.hlsl/.spv}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (spv)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/SPIRV/${BASE_NAME/.hlsl/.json}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (json)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/MSL/${BASE_NAME/.hlsl/.msl}"
+        ret=$?
+
+        if [ $ret -ne 0 ]; then
+            echo -e "Compile failed for $filename (msl)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/MSL/${BASE_NAME/.hlsl/.json}"
         $COMPILER "$filename" -o "${DST}/DXIL/${BASE_NAME/.hlsl/.dxil}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (dxil)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/DXIL/${BASE_NAME/.hlsl/.json}"
     fi
 done
@@ -20,10 +41,28 @@ for filename in $SRC/*.frag.hlsl; do
         echo -e "\e[94mCompile file: \e[94m${filename}\e[0m"
         BASE_NAME=$(basename ${filename})
         $COMPILER "$filename" -o "${DST}/SPIRV/${BASE_NAME/.hlsl/.spv}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (spv)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/SPIRV/${BASE_NAME/.hlsl/.json}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (json)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/MSL/${BASE_NAME/.hlsl/.msl}"
+        ret=$?
+
+        if [ $ret -ne 0 ]; then
+            echo -e "Compile failed for $filename (msl)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/MSL/${BASE_NAME/.hlsl/.json}"
         $COMPILER "$filename" -o "${DST}/DXIL/${BASE_NAME/.hlsl/.dxil}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (dxil)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/DXIL/${BASE_NAME/.hlsl/.json}"
     fi
 done
@@ -33,10 +72,28 @@ for filename in $SRC/*.comp.hlsl; do
         echo -e "\e[94mCompile file: \e[94m${filename}\e[0m"
         BASE_NAME=$(basename ${filename})
         $COMPILER "$filename" -o "${DST}/SPIRV/${BASE_NAME/.hlsl/.spv}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (spv)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/SPIRV/${BASE_NAME/.hlsl/.json}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (json)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/MSL/${BASE_NAME/.hlsl/.msl}"
+        ret=$?
+
+        if [ $ret -ne 0 ]; then
+            echo -e "Compile failed for $filename (msl)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/MSL/${BASE_NAME/.hlsl/.json}"
         $COMPILER "$filename" -o "${DST}/DXIL/${BASE_NAME/.hlsl/.dxil}"
+        if [ $? -ne 0 ]; then
+            echo -e "Compile failed for $filename (dxil)"
+            exit 1
+        fi
         $COMPILER "$filename" -o "${DST}/DXIL/${BASE_NAME/.hlsl/.json}"
     fi
 done
