42ab7c2fd5212a3c66153d78fbd8fea68b6ccb4f
Yogi Astawan <yogi.astawan@gmail.com>
2025-06-27 12:26:11 +0800

    fix gix_log_error

--- src/include/gix_engine/gix_log.h
+++ src/include/gix_engine/gix_log.h
@@ -8,12 +8,13 @@ extern "C" {
 #endif
 
 #ifdef BUILD_DEBUG
-#define gix_log_error(msg) \
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s. %s", msg, SDL_GetError())
-#define gix_log(msg...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, msg);
-#define gix_log_debug(msg...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, msg);
+#define gix_log_error(msg...)                        \
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, msg); \
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", SDL_GetError())
+#define gix_log(msg...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, msg)
+#define gix_log_debug(msg...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, msg)
 #else
-#define gix_log_error(msg)
+#define gix_log_error(msg...)
 #define gix_log(msg...)
 #define gix_log_debug(msg...)
 #endif
